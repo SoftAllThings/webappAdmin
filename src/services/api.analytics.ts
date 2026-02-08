76006194b31d84ff7baa6a9e55ebd94d9ffe2d57
fbotalla <botalla.fabrizio@gmail.com>
2026-02-08 23:21:00 +0100

    base url fix

--- src/services/api.analytics.ts
+++ src/services/api.analytics.ts
@@ -1,45 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
+import { API_BASE_URL } from "./api.config";
 
-
-export type Metric = 'users' | 'poops';
+export type Metric = "users" | "poops";
 
 export type AnalyticsPoint = {
   date: string;
   value: number;
-}
+};
 
 export type AnalyticsResponse = {
   data: AnalyticsPoint[];
   total: number;
   average: number;
-}
+};
 
 export async function fetchAnalytics(
   metric: Metric,
   from: string,
-  to: string
-): Promise<AnalyticsResponse>  {
- 
-    // const response = await fetch(
-    //   `http://localhost:3001/api/analytics?metric=${metric}&from=${from}&to=${to}`
-    // );
+  to: string,
+): Promise<AnalyticsResponse> {
+  // const response = await fetch(
+  //   `http://localhost:3001/api/analytics?metric=${metric}&from=${from}&to=${to}`
+  // );
 
-    // if (!response.ok) {
-    //   throw new Error('Failed to fetch analytics')
-    // }
+  // if (!response.ok) {
+  //   throw new Error('Failed to fetch analytics')
+  // }
 
-    // const json = await response.json();
+  // const json = await response.json();
 
+  //provato a fare con axios, mi piace di piu.
+  const fetchedData = await axios
+    .get(`${API_BASE_URL}/firebase/data`, {
+      params: {
+        metric,
+        from,
+        to,
+      },
+    })
+    .then((response) => response.data.data)
+    .catch((error) => {
+      throw new Error("Failed to fetch analytics: " + error.message);
+    });
 
-    //provato a fare con axios, mi piace di piu.
-    const fetchedData = await axios.get(`http://localhost:3001/api/firebase/data`, 
-      {params:{
-        metric, from, to
-    }})
-    .then(response => response.data.data)
-    .catch(error => {throw new Error('Failed to fetch analytics: ' + error.message)});
-
-    return fetchedData;
-  
+  return fetchedData;
 }
-
